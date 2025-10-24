7af2a413dd580263b5bc49e3ab1bfaa58a3c3dbe
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2025-10-24 09:50:53 +0200

    [ADD] Camera bounds lock into viewport
    edits to delete : gaetan map, to add pipou character state walk

--- Source/PipoumancienTeam2/Private/Camera/CameraCharacter.cpp
+++ Source/PipoumancienTeam2/Private/Camera/CameraCharacter.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Camera/CameraCharacter.h"
+
+#include "Camera/CameraComponent.h"
 #include "Camera/CameraWorldSubsystem.h"
 
 
@@ -25,23 +27,6 @@ void ACameraCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// FVector NewPosition = GetActorLocation();
-	//
-	// UCameraWorldSubsystem* prout = GetWorld()->GetSubsystem<UCameraWorldSubsystem>();
- //    if(prout == nullptr) return;
- //    
-	// FVector2D OutViewportBoundsMin = FVector2D::ZeroVector;
-	// FVector2D OutViewportBoundsMax = FVector2D::ZeroVector;
-	// prout->GetViewportBounds(OutViewportBoundsMin,OutViewportBoundsMax);
- //
-	// FVector min = FVector(OutViewportBoundsMin.X,OutViewportBoundsMin.Y,NewPosition.Z);
-	// FVector max = FVector(OutViewportBoundsMax.X,OutViewportBoundsMax.Y,NewPosition.Z);
-	// //NewPosition = FMath::Clamp(NewPosition,min,max);
- //
-	// NewPosition.X = FMath::Clamp(NewPosition.X, min.X, max.X);
-	// NewPosition.Y = FMath::Clamp(NewPosition.Y, min.Y, max.Y);
-	//
-	// this->SetActorLocation(NewPosition);
 }
 
 // Called to bind functionality to input
