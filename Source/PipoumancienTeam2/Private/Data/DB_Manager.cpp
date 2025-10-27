aa3c626df5b196f98b5b6519001766d064538831
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2025-10-27 11:59:13 +0100

    [ADD] singleton DB Manager

---
+++ Source/PipoumancienTeam2/Private/Data/DB_Manager.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/DB_Manager.h"
+
+UDB_Manager* UDB_Manager::MyInstance;
+
+UDB_Manager* UDB_Manager::Instance()
+{
+	if (!MyInstance)
+	{
+		MyInstance = NewObject<UDB_Manager>(); 
+	}
+
+	return MyInstance;
+}
+
+// F_Skeleton* UDB_Manager::GetSkeletonByID(int ID)
+// {
+// 	return nullptr;
+// }
---
+++ Source/PipoumancienTeam2/Public/Data/DB_Manager.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "DB_Manager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIPOUMANCIENTEAM2_API UDB_Manager : public UObject
+{
+	GENERATED_BODY()
+
+private:
+	static UDB_Manager* MyInstance;
+	
+public:
+	static UDB_Manager* Instance();
+	
+	//F_Skeleton* GetSkeletonByID(int ID);
+	
+
+};
