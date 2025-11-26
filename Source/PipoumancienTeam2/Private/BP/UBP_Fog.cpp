333db07cb4175d781ff3f7ca7c3b7fc8c015eaa8
nonogui <nonogui00@gmail.com>
2025-11-26 09:32:16 +0100

    fog

---
+++ Source/PipoumancienTeam2/Private/BP/UBP_Fog.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP/UBP_Fog.h"
---
+++ Source/PipoumancienTeam2/Public/BP/UBP_Fog.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "UBP_Fog.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIPOUMANCIENTEAM2_API UUBP_Fog : public UObject
+{
+	GENERATED_BODY()
+};
