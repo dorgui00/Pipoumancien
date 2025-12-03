da54dee20871ac80496c4a19ae94b26cb7407a85
nonogui <nonogui00@gmail.com>
2025-12-03 16:02:09 +0100

    fog

---
+++ Source/PipoumancienTeam2/Public/BP/Fog.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PNJ/SkeletonController.h"
+#include "Fog.generated.h"
+
+UCLASS()
+class PIPOUMANCIENTEAM2_API AFog : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFog();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Le mesh sur lequel appliquer le matériel
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Cube;
+
+	// Matériau de base (Fog)
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* FogMaterial;
+
+	// Instance dynamique
+	UPROPERTY()
+	UMaterialInstanceDynamic* DynMaterial;
+	
+	// Fonction équivalente à ton Custom Event "Suprimer Fog"
+	void SupprimerFog(F_Skeleton* Skeleton);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere)
+	int IDSkeleton;
+	
+};
