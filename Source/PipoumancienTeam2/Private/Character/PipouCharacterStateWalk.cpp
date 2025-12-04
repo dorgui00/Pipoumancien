256a425ff4a00f600a070cbd4bfe545d1f361bb3
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2025-12-04 11:38:45 +0100

    [FIX] merge

--- Source/PipoumancienTeam2/Private/Character/PipouCharacterStateWalk.cpp
+++ Source/PipoumancienTeam2/Private/Character/PipouCharacterStateWalk.cpp
@@ -23,25 +23,16 @@ void UPipouCharacterStateWalk::StateEnter(EPipouCharacterStateID PreviousStateID
 {
 	Super::StateEnter(PreviousStateID);
 	Character->GetMesh()->PlayAnimation(WalkAnim, true);
+	
 	// -------------- AUDIO -----------------
 	//walk steps audio start
-	if (WalkLoopSoundNecro && WalkLoopSoundDog)
+	if (WalkSoundBase)
 	{
-		if (AActor* Owner = GetOwner())
+		if (!IterateOnGround) //check if we're doing different sounds
 		{
-			if (Owner->GetName() == TEXT("BP_NecroCharacter0")) //verify if its necromancer or dog
-			{
-				WalkLoopComponent = UGameplayStatics::SpawnSoundAttached(
-					WalkLoopSoundNecro,
-					Character->GetRootComponent());
-
-			}
-			else {
-
-				WalkLoopComponent = UGameplayStatics::SpawnSoundAttached(
-					WalkLoopSoundDog,
-					Character->GetRootComponent());
-			}
+			WalkLoopComponent = UGameplayStatics::SpawnSoundAttached(
+				WalkSoundBase,
+				Character->GetRootComponent());
 		}
 	}
 
@@ -109,6 +100,43 @@ void UPipouCharacterStateWalk::StateTick(float Deltatime)
 		}
 	}
 
+	//AUDIO
+	if (IterateOnGround)
+	{
+		bool bIsMoving = (Character->GetInputMoveXY().SquaredLength() > Character->DeadZone * Character->DeadZone);
+
+		if (bIsMoving) // only walk if actually moving
+		{
+			FootstepTimer += Deltatime;
+
+			if (FootstepTimer >= FootstepInterval)
+			{
+				FootstepTimer = 0.f;
+
+				FHitResult Hit;
+				FCollisionQueryParams Params;
+				Params.AddIgnoredActor(Character);
+				Params.bReturnPhysicalMaterial = true;
+
+				FVector Start = Character->GetActorLocation();
+				FVector End = Start - FVector(0, 0, 200.f);
+
+				if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_WorldStatic, Params))
+				{
+
+					if (UPhysicalMaterial* PhysMat = Hit.PhysMaterial.Get())
+					{
+						PlayFootstepsSound(PhysMat);
+					}
+				}
+			}
+		}
+		else
+		{
+			// standing still
+			FootstepTimer = 0.f;
+		}
+	}
 }
 
 void UPipouCharacterStateWalk::StateExit(EPipouCharacterStateID NextStateID)
@@ -136,4 +164,29 @@ void UPipouCharacterStateWalk::OnCharacterPressedNote(UInputAction* InputAction)
 	//Super::OnCharacterPressedNote(InputAction);
 
 	//Character->GetMesh()->PlayAnimation(MusicWalkAnim,false);
+}
+
+// ------- AUDIO --------
+void UPipouCharacterStateWalk::PlayFootstepsSound(UPhysicalMaterial* PhysMat)
+{
+	if (PhysMat->SurfaceType == SurfaceType1) // grass
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, GrassFootstepSound, Character->GetActorLocation());
+	}
+	else if (PhysMat->SurfaceType == SurfaceType2) // dirt
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, DirtFootstepSound, Character->GetActorLocation());
+	}
+	else if (PhysMat->SurfaceType == SurfaceType3) // townRoad
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, TownRoadFootstepSound, Character->GetActorLocation());
+	}
+	else if (PhysMat->SurfaceType == SurfaceType4) // snow
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, SnowFootstepSound, Character->GetActorLocation());
+
+	} else {
+
+		return;
+	}
 }
