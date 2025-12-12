1a2136a9a8d152028f04787d26e374e32f711c45
Gaëtan Decisy <gaetan.decisy6@hotmail.com>
2025-12-12 16:15:40 +0100

    [TryToFix] Miasme

--- Source/PipoumancienTeam2/Private/UI/GlobalHUDSubsystem.cpp
+++ Source/PipoumancienTeam2/Private/UI/GlobalHUDSubsystem.cpp
@@ -406,8 +406,12 @@ void UGlobalHUDSubsystem::InitMistakeMaterial()
 		
 		CurrentMistakeMaterialInstance->GetScalarParameterValue(FHashedMaterialParameterInfo(TEXT("Radius")), CurrentMaterialRadius);
 		CurrentMistakeMaterialInstance->GetScalarParameterValue(FHashedMaterialParameterInfo(TEXT("Thickness")), CurrentMaterialThickness);
+		
 		TargetMaterialRadius = MaxRadius;
 		TargetMaterialThickness = MinThickness;
+		
+		CurrentMistakeMaterialInstance->SetScalarParameterValue(TEXT("Radius"), MinRadius);
+		CurrentMistakeMaterialInstance->SetScalarParameterValue(TEXT("Thickness"), MinThickness);
 	}
 }
 
