8ae6a90a07354ae366cb2ce0aa7da7518f3c31b9
nonogui <nonogui00@gmail.com>
2025-12-16 12:18:02 +0100

    dialoge

--- Source/PipoumancienTeam2/Private/PNJ/SkeletonController.cpp
+++ Source/PipoumancienTeam2/Private/PNJ/SkeletonController.cpp
@@ -222,7 +222,7 @@ void ASkeletonController::InterationDialogue()
 {
 	if (!WidgetComponent->IsVisible())
 	{
-		WidgetComponent->SetVisibility(false);			
+		WidgetComponent->SetVisibility(true);			
 	}
 }
 
