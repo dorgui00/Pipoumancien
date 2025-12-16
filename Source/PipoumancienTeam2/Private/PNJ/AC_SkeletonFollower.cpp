61587fa7205699355fb637ed4fe8937ea3d4d479
pikaillott <40119956+pikaillott@users.noreply.github.com>
2025-12-16 11:40:56 +0100

    [ADDED] animation outro

--- Source/PipoumancienTeam2/Private/PNJ/AC_SkeletonFollower.cpp
+++ Source/PipoumancienTeam2/Private/PNJ/AC_SkeletonFollower.cpp
@@ -376,11 +376,11 @@ void UAC_SkeletonFollower::TickFollowSpline(float DeltaTime)
     if (CurrentDistance >= SplineLength - KINDA_SMALL_NUMBER)
     {
         bFollowingSpline = false;
-        UE_LOG(LogTemp, Display, TEXT("[SkeletonFollower] %s reached the end of spline."), *ParentActor->GetName());
 
         if (bOnVillageSpline)
         {
             OnReachHome.Broadcast();
+            UE_LOG(LogTemp, Display, TEXT("[SkeletonFollower] %s reached his home."), *ParentActor->GetName());
         }
     }
 }
